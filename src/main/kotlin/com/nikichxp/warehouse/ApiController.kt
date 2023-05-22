d05c29465b8b1882e5fcd57ff6c641a7074a6635
Nikita Ruzhevsky <nikichx2@gmail.com>
2023-05-22 16:28:10 +0200

    updated controller

--- src/main/kotlin/com/nikichxp/warehouse/ApiController.kt
+++ src/main/kotlin/com/nikichxp/warehouse/ApiController.kt
@@ -1,17 +1,28 @@
 package com.nikichxp.warehouse
 
-import java.io.FileNotFoundException
+import com.mongodb.client.result.UpdateResult
+import java.util.Optional
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactor.awaitSingle
 import org.bson.types.ObjectId
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.find
 import org.springframework.data.mongodb.core.mapping.Field
 import org.springframework.data.mongodb.core.mapping.FieldType
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
+import org.springframework.data.mongodb.core.update
+import org.springframework.data.repository.NoRepositoryBean
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpClientErrorException.NotFound
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.awaitBody
 import org.springframework.web.reactive.function.server.bodyAndAwait
@@ -27,7 +38,11 @@ class ApiController(
     fun router() = coRouter {
         path("storage").nest {
             GET("/list") {
-                ServerResponse.ok().bodyAndAwait(storageService.getAllSKUs())
+                val showEmpty = it.queryParam("showEmpty").map { it.toBoolean() }
+                val options = ListOptions(
+                    showEmpty = showEmpty
+                )
+                ServerResponse.ok().bodyAndAwait(storageService.listSKUs(options))
             }
             GET("/{id}") {
                 val id = ObjectId(it.pathVariable("id"))
@@ -36,40 +51,66 @@ class ApiController(
             PUT("/{id}") {
                 val id = ObjectId(it.pathVariable("id"))
                 val quantity: Int = it.queryParam("quantity").map { i -> i.toIntOrNull() }.orElseThrow()!!
-                ServerResponse.ok().bodyValueAndAwait(storageService.updateQuantity(id, quantity))
+                storageService.updateQuantity(id, quantity)
+                ServerResponse.ok().bodyValueAndAwait("Success")
             }
             POST("/") {
                 ServerResponse.ok().bodyValueAndAwait(storageService.addSKU(it.awaitBody()))
             }
-            onError<FileNotFoundException>() { err, _ ->
-                status(404).bodyValueAndAwait(mapOf("errorMessage" to err.message))
+        }
+        onError<Exception> { err, _ ->
+            when (err) {
+                is ElementNotFoundException -> status(404).bodyValueAndAwait(ExceptionInfo(err))
+                else -> status(503).bodyValueAndAwait(ExceptionInfo(err))
             }
         }
     }
 }
 
+data class ListOptions(
+    val showEmpty: Optional<Boolean> = Optional.empty()
+) {
+    fun toQuery(): Query {
+        val query = Query()
+        if (!this.showEmpty.get()) {
+            query.addCriteria(Criteria.where(SKU::quantity.name).gt(0))
+        }
+        return query
+    }
+}
+
+data class ExceptionInfo(
+    val message: String,
+    val errorType: String
+) {
+    constructor(exception: Throwable) : this(exception.message ?: "No info provided", exception.javaClass.simpleName)
+}
+
+class ElementNotFoundException(query: Any) : Exception("Element not found by query: $query")
+
 @Service
 class StorageService(
-    private val repository: SKURepository
+    private val dao: SKUDao
 ) {
 
     suspend fun addSKU(request: NewSKURequest): SKU {
-        return repository.save(SKU(name = request.name, quantity = request.quantity))
+        return dao.save(SKU(name = request.name, quantity = request.quantity))
     }
 
     suspend fun getById(id: ObjectId): SKU {
-        return repository.findById(id) ?: throw FileNotFoundException()
+        return dao.findById(id) ?: throw ElementNotFoundException(id)
     }
 
-    fun getAllSKUs(): Flow<SKU> {
-        return repository.findAll()
+    fun listSKUs(parameters: ListOptions): Flow<SKU> {
+        return dao.customFind(parameters)
     }
 
-    suspend fun updateQuantity(id: ObjectId, quantity: Int): SKU {
-        return repository.findById(id)?.let {
-            it.quantity = quantity
-            repository.save(it)
-        } ?: throw FileNotFoundException()
+    suspend fun updateQuantity(id: ObjectId, quantity: Int) {
+        val result = dao.updateQuantity(id, quantity)
+        val success = result.modifiedCount > 0
+        if (!success) {
+            throw ElementNotFoundException(id)
+        }
     }
 }
 
@@ -88,3 +129,28 @@ data class SKU(
 }
 
 interface SKURepository : CoroutineCrudRepository<SKU, ObjectId>
+
+interface SKUCustomRepo {
+    fun customFind(parameters: ListOptions): Flow<SKU>
+    suspend fun updateQuantity(id: ObjectId, quantity: Int): UpdateResult
+}
+
+@Repository
+interface SKUDao : SKURepository, SKUCustomRepo
+
+@Repository
+class SKUCustomRepoImpl(
+    private val mongoTemplate: ReactiveMongoTemplate
+) : SKUCustomRepo {
+    override fun customFind(parameters: ListOptions): Flow<SKU> {
+        val query = parameters.toQuery()
+        return mongoTemplate.find<SKU>(query).asFlow()
+    }
+
+    override suspend fun updateQuantity(id: ObjectId, quantity: Int): UpdateResult {
+        return mongoTemplate.update<SKU>().matching(Criteria.where(SKU::id.name).`is`(id))
+            .apply(Update.update(SKU::quantity.name, quantity))
+            .first()
+            .awaitSingle()
+    }
+}
