934e629d954129a5a601570ed6abbc3ec13df8bc
Nikita Ruzhevsky <nikichx2@gmail.com>
2023-07-12 14:50:02 +0200

    work in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+function getWarehouseContent() {
+    console.log("request sent")
+    fetch("127.0.0.1:8080/storage/list")
+        .then(res => {
+            res.body.getReader().read()
+                .then(r => r.value)
+                .then(arr => {
+                    var i, str = '';
+
+                    for (i = 0; i < arr.length; i++) {
+                        str += '%' + ('0' + arr[i].toString(16)).slice(-2);
+                    }
+                    str = decodeURIComponent(str);
+                    console.log(str)
+                })
+            // return res.json()
+        })
+    // .then(json => console.log(json))
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello app</h1>
+            <button onClick={getWarehouseContent}>Click</button>
+        </div>
+    );
 }
 
 export default App;
