91ca991e032667363d0ffafeddc6b18bf261ada2
Nikita <nikichx2@gmail.com>
2024-03-18 18:53:54 +0100

    filtration by user; http examples

--- src/main/kotlin/com/nikichxp/warehouse/ApiController.kt
+++ src/main/kotlin/com/nikichxp/warehouse/ApiController.kt
@@ -1,8 +1,11 @@
 package com.nikichxp.warehouse
 
 import com.mongodb.client.result.UpdateResult
-import java.util.Optional
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flattenConcat
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactor.awaitSingle
@@ -26,6 +29,7 @@ import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.awaitBody
 import org.springframework.web.reactive.function.server.bodyAndAwait
@@ -44,9 +48,14 @@ class CorsGlobalConfiguration : WebFluxConfigurer {
     }
 }
 
+fun getUser(serverRequest: ServerRequest): String {
+    return serverRequest.attributes()["user"] as String
+}
+
 @Configuration
 class ApiController(
     private val storageService: StorageService,
+    private val storageTagsService: StorageTagsService,
     private val conversionService: ConversionService
 ) {
 
@@ -56,10 +65,22 @@ class ApiController(
             ServerResponse.ok().bodyValueAndAwait("pong!~")
         }
         path("storage").nest {
+            filter { request, next ->
+                request.headers().header("user").firstOrNull()?.let {
+                    request.attributes()["user"] = it
+                    next(request)
+                } ?: ServerResponse.badRequest().bodyValueAndAwait("Missing user header")
+            }
+            path("/tags").nest {
+                GET("/list") {
+                    ServerResponse.ok().bodyAndAwait(storageTagsService.listPossibleTags(getUser(it)))
+                }
+            }
             GET("/list") { req ->
-                val showEmpty = req.queryParam("showEmpty").map { it.toBoolean() }
+                val showEmpty = req.queryParam("showEmpty").map { it.toBoolean() }.orElse(null)
                 val options = ListOptions(
-                    showEmpty = showEmpty
+                    showEmpty = showEmpty,
+                    userId = getUser(req)
                 )
                 ServerResponse.ok().bodyAndAwait(
                     storageService
@@ -69,20 +90,26 @@ class ApiController(
             }
             GET("/{id}") {
                 val id = ObjectId(it.pathVariable("id"))
-                ServerResponse.ok().bodyValueAndAwait(storageService.getById(id))
+                val userId = getUser(it)
+                val responseEntity = storageService.getByIdAndUserId(id, userId)
+                ServerResponse.ok().bodyValueAndAwait(responseEntity)
             }
             DELETE("/{id}") {
                 val id = ObjectId(it.pathVariable("id"))
-                ServerResponse.ok().bodyValueAndAwait(storageService.deleteById(id))
+                val userId = getUser(it)
+                ServerResponse.ok().bodyValueAndAwait(storageService.deleteById(id, userId))
             }
             PUT("/{id}") {
                 val id = ObjectId(it.pathVariable("id"))
                 val quantity: Int = it.queryParam("quantity").map { i -> i.toIntOrNull() }.orElseThrow()!!
-                storageService.updateQuantity(id, quantity)
+                val userId = getUser(it)
+                storageService.updateQuantity(id, quantity, userId)
                 ServerResponse.ok().bodyValueAndAwait("Success")
             }
             POST("/") {
-                ServerResponse.ok().bodyValueAndAwait(storageService.addSKU(it.awaitBody()))
+                val userId = getUser(it)
+                val createdEntity = storageService.addSKU(it.awaitBody(), userId)
+                ServerResponse.ok().bodyValueAndAwait(conversionService.convert(createdEntity, SKUDTO::class.java)!!)
             }
         }
         onError<Exception> { err, _ ->
@@ -95,13 +122,14 @@ class ApiController(
 }
 
 data class ListOptions(
-    val showEmpty: Optional<Boolean> = Optional.empty()
+    val showEmpty: Boolean? = null,
+    var userId: String? = null
 ) {
+
     fun toQuery(): Query {
         val query = Query()
-        if (!this.showEmpty.orElse(true)) {
-            query.addCriteria(Criteria.where(SKU::quantity.name).gt(0))
-        }
+        userId?.let { query.addCriteria(Criteria.where(SKU::userIds.name).`is`(it)) }
+        showEmpty?.let { query.addCriteria(Criteria.where(SKU::quantity.name).gt(0)) }
         return query
     }
 }
@@ -115,25 +143,48 @@ data class ExceptionInfo(
 
 class ElementNotFoundException(query: Any) : Exception("Element not found by query: $query")
 
+@OptIn(ExperimentalCoroutinesApi::class)
+@Service
+class StorageTagsService(
+    private val dao: SKUDao
+) {
+    suspend fun listPossibleTags(userId: String): Flow<String> {
+        return dao.findAll().map { it.tags.asFlow() }.flattenConcat().distinct()
+    }
+}
+
+fun <T> Flow<T>.distinct(): Flow<T> = flow {
+    val past = mutableSetOf<T>()
+    collect {
+        val isNew = past.add(it)
+        if (isNew) emit(it)
+    }
+}
+
 @Service
 class StorageService(
     private val dao: SKUDao
 ) {
 
-    suspend fun addSKU(request: NewSKURequest): SKU {
-        return dao.save(SKU(name = request.name, quantity = request.quantity))
+    suspend fun addSKU(request: NewSKURequest, userId: String): SKU {
+        return dao.save(
+            SKU(
+                name = request.name,
+                quantity = request.quantity,
+                tags = request.tags
+            ).also { it.userIds += userId })
     }
 
-    suspend fun getById(id: ObjectId): SKU {
-        return dao.findById(id) ?: throw ElementNotFoundException(id)
+    suspend fun getByIdAndUserId(id: ObjectId, userId: String): SKU {
+        return dao.findByIdAndUserIds(id, userId) ?: throw ElementNotFoundException(id)
     }
 
     fun listSKUs(parameters: ListOptions): Flow<SKU> {
         return dao.customFind(parameters)
     }
 
-    suspend fun deleteById(id: ObjectId): Boolean {
-        val item = dao.findById(id) ?: throw ElementNotFoundException(id)
+    suspend fun deleteById(id: ObjectId, userId: String): Boolean {
+        val item = dao.findByIdAndUserIds(id, userId) ?: throw ElementNotFoundException(id)
         return if (item.quantity > 0) {
             false
         } else {
@@ -142,8 +193,8 @@ class StorageService(
         }
     }
 
-    suspend fun updateQuantity(id: ObjectId, quantity: Int) {
-        val result = dao.updateQuantity(id, quantity)
+    suspend fun updateQuantity(id: ObjectId, quantity: Int, userId: String) {
+        val result = dao.updateQuantity(id, quantity, userId)
         val success = result.modifiedCount > 0
         if (!success) {
             throw ElementNotFoundException(id)
@@ -151,16 +202,14 @@ class StorageService(
     }
 }
 
-data class NewSKURequest(val name: String, val quantity: Int)
+data class NewSKURequest(val name: String, val quantity: Int, val tags: Set<String> = setOf())
 
 data class SKUDTO(
     val id: String,
     val name: String,
     val quantity: Int,
     val tags: Set<String>
-) {
-    val isDTO = true
-}
+)
 
 @Component
 class SKUConverter : Converter<SKU, SKUDTO> {
@@ -185,13 +234,18 @@ data class SKU(
     @Field(targetType = FieldType.OBJECT_ID)
     lateinit var id: String
 
+    var userIds: Set<String> = setOf()
+
 }
 
-interface SKURepository : CoroutineCrudRepository<SKU, ObjectId>
+interface SKURepository : CoroutineCrudRepository<SKU, ObjectId> {
+    @Suppress("SpringDataRepositoryMethodParametersInspection", "SpringDataRepositoryMethodReturnTypeInspection")
+    suspend fun findByIdAndUserIds(id: ObjectId, userId: String): SKU?
+}
 
 interface SKUCustomRepo {
     fun customFind(parameters: ListOptions): Flow<SKU>
-    suspend fun updateQuantity(id: ObjectId, quantity: Int): UpdateResult
+    suspend fun updateQuantity(id: ObjectId, quantity: Int, userId: String): UpdateResult
 }
 
 @Repository
@@ -207,8 +261,11 @@ class SKUCustomRepoImpl(
         return mongoTemplate.find<SKU>(query).asFlow()
     }
 
-    override suspend fun updateQuantity(id: ObjectId, quantity: Int): UpdateResult {
-        return mongoTemplate.update<SKU>().matching(Criteria.where(SKU::id.name).`is`(id))
+    override suspend fun updateQuantity(id: ObjectId, quantity: Int, userId: String): UpdateResult {
+        return mongoTemplate.update<SKU>().matching(
+            Criteria.where(SKU::id.name).`is`(id)
+                .and(SKU::userIds.name).`is`(userId)
+        )
             .apply(Update.update(SKU::quantity.name, quantity))
             .first()
             .awaitSingle()
